dc9dabdd968bee642aa8a5af569e010de73d5ae5
Arshanes <valentin.arshanes.thomas@gmail.com>
2014-01-21 10:48:52 +0100

    Ajouts fichiers

---
+++ vues/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Demo</title>
+		<link rel="stylesheet" type="text/css" href="../asset/css/bootstrap.css"/>
+		<!--<link rel="stylesheet" type="text/css" href="../asset/css/style.css"/>-->
+		<script type="text/javascript" src="../asset/css/bootstrap.css"></script>
+	</head>
+	<body>
+		<!-- HEADER-->
+		<header class="row-fluid">
+			<div class="span12">
+				<!--Première section du header-->
+				<div class="row-fluid">
+					<div class="span3">
+						<h1>Projet M2L3</h1>
+					</div>
+					<!--Bloc Menu -->
+					<nav class="span5 navbar">
+						<div class="navbar-inner">
+							<a class="brand" href="#">Accueil</a>
+							<ul class="nav">
+								<li class="active"><a href="#">Gestion des patterns</a></li>
+								<li><a href="#">Administration</a></li>
+								<li><a href="#">Link</a></li>
+							</ul>
+						</div>
+					</nav>
+					<!--Bloc login -->
+					<div class="span4">
+						    <form class="form-inline">
+							    <input type="text" class="input-small" placeholder="Login">
+							    <input type="password" class="input-small" placeholder="Password">
+							    <button type="submit" class="btn">Connexion</button>
+							    <a>Creer un compte</a>
+							</form>
+					</div>
+				</div>
+				<!--Fil d'ariane-->
+				<div class="row-fluid">
+					<div class="span12">
+						<u>Accueil</u> > <strong>Gestion pattern</strong>
+					</div>
+				</div>
+			</div>
+		</header>
