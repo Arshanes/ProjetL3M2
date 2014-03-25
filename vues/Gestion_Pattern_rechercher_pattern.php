f5a2af35ef69812d383413be140aaedf19144372
YannDeriat <naydoe@hotmail.fr>
2014-03-25 10:01:07 +0100

    Caca

--- vues/Gestion_Pattern_rechercher_pattern.php
+++ vues/Gestion_Pattern_rechercher_pattern.php
@@ -1,3 +1,16 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Projet L3/M2 - Gestion pattern_Recherche de pattern</title>
+		<link rel="stylesheet" type="text/css" href="../asset/css/bootstrap.css"/>
+		<!--<link rel="stylesheet" type="text/css" href="../asset/css/style.css"/>-->
+		<script type="text/javascript" src="../asset/js/calendrier.js"></script>
+	</head>
+	<body>
+
 <!--CONTENU-->
 <div class="row-fluid">
 	<p class="divCenter">Que souhaitez-vous faire?</p>
