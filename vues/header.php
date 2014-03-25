94b15254e2c041af00b65efed0b405549a708358
Arshanes <valentin.arshanes.thomas@gmail.com>
2014-03-25 09:21:46 +0100

    MaJ

--- vues/header.php
+++ vues/header.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Projet L3M2</title>
-		<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css"/>
-		<link rel="stylesheet" type="text/css" href="asset/css/style.css"/>
-	</head>
-	<body>
 		<!-- HEADER-->
 		<header class="row-fluid">
 			<div class="span12">
@@ -19,24 +9,18 @@
 					<!--Bloc Menu -->
 					<nav>
 						<ul>
-							<li><a href="#">Accueil</a></li>
-							<li><a href="#">Gestion des patterns</a></li>
+							<li><a href="index.php">Accueil</a></li>
+							<li><a href="gestionPattern.php">Gestion des patterns</a></li>
 							<li><a href="#">Administration</a></li>
 						</ul>
 					</nav>
 					<!--Bloc login -->
 					<div id="blocLog">
 					    <form class="form-inline">
-<<<<<<< HEAD
 						    <input type="text" class="input-small" placeholder="Login">
 						    <input type="password" class="input-small" placeholder="Password">
 						    <button type="submit" class="submit" id="connexion">Connexion</button>
-=======
-						    <input type="text" name="login" class="input-small" placeholder="Login"/>
-						    <input type="password" name="password" class="input-small" placeholder="Password"/>
-						    <button type="submit" class="submit">Connexion</button>
->>>>>>> fb02c0d95cc474c5272b237c4a2ecf57d6619f8b
-						    <a>Creer un compte</a>
+						    <a href="inscription.php">Creer un compte</a>
 						</form>
 					</div>
 				</div>
